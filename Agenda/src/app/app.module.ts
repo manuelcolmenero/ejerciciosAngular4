3dc810bc776cc04bdf999529c1edb56157279def
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-06-15 21:23:29 +0200

    Actualización por revisión de Módulos

--- Agenda/src/app/app.module.ts
+++ Agenda/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // En declarations se indican los componentes, pipes y 
+  // directivas de la aplicación
   declarations: [
     AppComponent
   ],
+  // En imports se indican módulos de los cuáles se depende
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // En providers se indican los proveedores de todas aquellas
+  // piezas que sean susceptibles de ser inyectables en la app
+  providers: [
+
+  ],
+  // En bootstrap se indica el componente raíz, que es el primero
+  // que se instanciará y hará lo propio con el resto
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
