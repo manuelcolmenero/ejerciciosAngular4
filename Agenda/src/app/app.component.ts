37d1e2d39d43d13603c00364a64880763b28b268
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-06-15 22:43:36 +0200

    Lecciones Sesión 1 - 15-06-2017 * Comienzo con app Agenda * Creación del módulo llista-contactos

--- Agenda/src/app/app.component.ts
+++ Agenda/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // En 'selector' se indica el elemento HTML donde se instanciará 
+  // dicho componente
   selector: 'app-root',
+  // En 'templateUrls' se indica la ruta al template del componente.
+  // Se puede escribir inline en la propiedad 'template'
+  // ejemplo: 
+  // template: "<h1>Hola mundo</h1>",
   templateUrl: './app.component.html',
+  // En 'stylesUrls' se indican las rutas de los documentos CSS que
+  // contienen los estilos que afectan a este componente.
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mi Super Aplicación Angular';
+  
+  contactos: string[] = [
+    'Son Goku',
+    'Megaman',
+    'Spider-Man',
+    'Sheldon Cooper',
+    'Chicho Terremoto'
+
+  ];
+
+// Se implementa la función OnInit
+  ngOnInit () {
+    console.log('I am Groot!');
+  }
 }
