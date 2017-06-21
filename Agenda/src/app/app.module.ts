fef02bb306b880646b7cb28a76bb47217f077a67
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-06-21 22:33:54 +0200

    Lección 21-06-2017 - Sesión 1 * Routing y Single-Page

--- Agenda/src/app/app.module.ts
+++ Agenda/src/app/app.module.ts
@@ -1,12 +1,17 @@
+// import propio Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { HttpModule } from '@angular/http'; 
+import { AppRoutingModule } from "app/app-routing/app-routing.module";
 
+// import propio y de terceros
 import { AppComponent } from './app.component';
 import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 import { ContactosService } from "app/contactos.service";
 import { FormularioAltaComponent } from './formulario-alta/formulario-alta.component';
+import { MisContactosComponent } from './mis-contactos/mis-contactos.component';
+import { NuevoContactoComponent } from './nuevo-contacto/nuevo-contacto.component';
 
 @NgModule({
   // En declarations se indican los componentes, pipes y 
@@ -14,13 +19,16 @@ import { FormularioAltaComponent } from './formulario-alta/formulario-alta.compo
   declarations: [
     AppComponent,
     ListaContactosComponent,
-    FormularioAltaComponent
+    FormularioAltaComponent,
+    MisContactosComponent,
+    NuevoContactoComponent
   ],
   // En imports se indican módulos de los cuáles se depende
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   // En providers se indican los proveedores de todas aquellas
   // piezas que sean susceptibles de ser inyectables en la app
