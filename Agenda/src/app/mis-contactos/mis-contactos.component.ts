bfb77ff272c7a27f264e2873ab1e8a751e39727b
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-06-21 23:18:56 +0200

    Lección 21-06-2017 - Sección 2 * Routing - fase 2 * Montar pestañas y re-ajuste código

--- Agenda/src/app/mis-contactos/mis-contactos.component.ts
+++ Agenda/src/app/mis-contactos/mis-contactos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+
+import { ContactosService } from "../contactos.service";
+import { Contacto } from '../contacto';
 
 @Component({
   selector: 'app-mis-contactos',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisContactosComponent implements OnInit {
 
-  constructor() { }
+  // Atributo a nivel de instancia
+  contactos$: Observable<Contacto[]>;
+
+  // Para hacer una inyección de dependencias se necesita hacerlo
+  // en el constructor de una clase. Se ha de indicar un 
+  // párametro con un modificador de acceso (obligatorio).
+  // Además, se ha de anotar su tipo, que no es otro que el
+  // servicio a inyectar.
+  constructor (private _contactosService: ContactosService) { }
 
   ngOnInit() {
+    // Se llama al servicio de contactos para obtener la lista
+    // Para ello se mueve lo que devuelva 'obtenerContactos' en 
+    // la variable de contactos (lista contactos que se está pintando).
+    this.contactos$ = this._contactosService.obtenerContactos();
   }
 
 }
--- Agenda/src/app/nuevo-contacto/nuevo-contacto.component.ts
+++ Agenda/src/app/nuevo-contacto/nuevo-contacto.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { ContactosService } from "../contactos.service";
+import { Contacto } from "../contacto";
 
 @Component({
   selector: 'app-nuevo-contacto',
   templateUrl: './nuevo-contacto.component.html',
   styleUrls: ['./nuevo-contacto.component.css']
 })
-export class NuevoContactoComponent implements OnInit {
+export class NuevoContactoComponent {
 
-  constructor() { }
+  constructor (
+    private _contactosService: ContactosService,
+    private _router: Router) { }
 
-  ngOnInit() {
+  guardarContacto(contacto: Contacto): void {
+    this._contactosService
+      .altaContacto(contacto)
+      .subscribe(() => {
+        alert ('El contacto se ha creado satisfactoriamente :-)');
+        this._router.navigate(['/mis-contactos']);
+      });
   }
-
 }
